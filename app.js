78d54743a754e624048d8b273e9090fb6f0f5565
jfrancomb-spec <jfrancomb@gmail.com>
2026-03-02 07:54:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://apjdknqppglnamndwwya.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_JwsPcXgi_T-NQZo1tGZY_w_kcRc9kWc";
 
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
